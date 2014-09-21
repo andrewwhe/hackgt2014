86c77e9df4147955759bd5ac9ffaaec9c5dede28
andrewwhe <andrew@andrewhe.me>
2014-09-20 22:07:30 -0400

    added clickable button functionality to navbar

--- extension/app/js/page.js
+++ extension/app/js/page.js
@@ -1,5 +1,21 @@
 $(".sbutton").click(
-  function(this) {
-    alert(this);    
+  function() {
+    switch(this.id) {
+      case 'o1':
+        $("#o1").addClass("selected");
+        $("#o2").removeClass("selected");
+        $("#o3").removeClass("selected");
+        break;
+      case 'o2':
+        $("#o1").removeClass("selected");
+        $("#o2").addClass("selected");
+        $("#o3").removeClass("selected");
+        break;
+      case 'o3':
+        $("#o1").removeClass("selected");
+        $("#o2").removeClass("selected");
+        $("#o3").addClass("selected");
+        break;
+    }
   }
 );
